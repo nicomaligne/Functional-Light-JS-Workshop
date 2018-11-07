5f2d9611596bec1c9f5d4188a65e7b753c19f129
nicomaligne <nmaligne@gmail.com>
2018-11-07 14:02:28 +0100

    exo 6

--- exercises/ex6/ex6.js
+++ exercises/ex6/ex6.js
@@ -1,7 +1,16 @@
-function mult(x,y,z) {
-	return x * y * z;
+function mult(x, y, z) {
+  return x * y * z;
 }
 
-mult(3,4,5);	// 60
+function multRec(result, first, ...args) {
+  if (args.length === 0) {
+    return result * first;
+  }
+  return result * multRec(first, ...args);
+}
 
-mult(3,4,5,6);	// Oops!
+let resultRec = multRec(3, 4, 5); // 60
+console.log({ resultRec });
+
+resultRec = multRec(3, 4, 5, 6); // Oops!
+console.log({ resultRec })
